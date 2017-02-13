44b849086835976fecd95bc53c78a3f1e0d8128c
yuri <yurisson7@gmail.com>
2017-02-13 11:02:47 +0200

    added 3 tests TVchanels

---
+++ src/test/java/com/accenture/ava/AvaTVChanelsTest.java
@@ -0,0 +1,65 @@
+package com.accenture.ava;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsArrayContaining.hasItemInArray;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.klarna.hiverunner.HiveShell;
+import com.klarna.hiverunner.StandaloneHiveRunner;
+import com.klarna.hiverunner.annotations.HiveRunnerSetup;
+import com.klarna.hiverunner.annotations.HiveSQL;
+import com.klarna.hiverunner.annotations.HiveSetupScript;
+import com.klarna.hiverunner.config.HiveRunnerConfig;
+
+@RunWith(StandaloneHiveRunner.class)
+public class AvaTVChanelsTest {
+    @HiveSQL(files = {"sql/tv_chanels.hql",
+            "sql/tvchanels_rejected.hql",
+            "sql/tvchannels_staging.hql"}, autoStart = false)
+    private HiveShell hiveShell;
+
+    @HiveRunnerSetup
+    public final HiveRunnerConfig CONFIG = new HiveRunnerConfig() {
+        {
+            setHiveExecutionEngine("mr");
+        }
+    };
+
+    @HiveSetupScript
+    private String setup = "set hive.support.sql11.reserved.keywords=false; " +
+            "SET hive.exec.dynamic.partition = true; SET hive.exec.dynamic.partition.mode = nonstrict; " +
+            "SET hive.mapred.mode = nonstrict;";
+
+    @Before
+    public void setup() {
+        hiveShell.setHiveConfValue("ROOTPATH", "${hiveconf:hadoop.tmp.dir}");
+        hiveShell.setHiveConfValue("ENDDATE", "20080815");
+        hiveShell.start();
+    }
+
+    @Test
+    public void testLoadFilePurchase() {
+        String[] actual = hiveShell.executeQuery("SELECT * FROM tvchannels").toArray(new String[0]);
+        Assert.assertEquals(5, actual.length);
+    }
+
+    @Test
+    public void testTablesInListIsCreated() {
+        String[] actual = hiveShell.executeQuery("SHOW TABLES").toArray(new String[0]);
+        assertThat(actual, hasItemInArray("tvchannels"));
+        assertThat(actual, hasItemInArray("tvchannels_rejected"));
+        assertThat(actual, hasItemInArray("tvchannels_staging"));
+
+    }
+
+    @Test
+    public void testTVChanelsPartition() {
+        String[] actual = hiveShell.executeQuery("SELECT * FROM tvchannels t WHERE t.partition_date='20080815'")
+                .toArray(new String[0]);
+        assertThat(actual, hasItemInArray("300000005\tCHANNEL_5\tLIVE\tSPORT\tCOMEDY\t5\tBBC\t20080815"));
+    }
+}
