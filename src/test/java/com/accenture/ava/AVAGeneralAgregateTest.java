48c0a07da17a2befd00f7741e1036815269b5221
yuris7 <yurisson7@gmail.com>
2017-02-16 16:29:12 +0200

    added assertions to weekly & monthly

--- src/test/java/com/accenture/ava/AVAGeneralAgregateTest.java
+++ src/test/java/com/accenture/ava/AVAGeneralAgregateTest.java
@@ -1,16 +1,16 @@
 package com.accenture.ava;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import com.klarna.hiverunner.HiveShell;
 import com.klarna.hiverunner.StandaloneHiveRunner;
 import com.klarna.hiverunner.annotations.HiveRunnerSetup;
 import com.klarna.hiverunner.annotations.HiveSQL;
 import com.klarna.hiverunner.annotations.HiveSetupScript;
 import com.klarna.hiverunner.config.HiveRunnerConfig;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 @RunWith(StandaloneHiveRunner.class)
 public class AVAGeneralAgregateTest {
@@ -49,79 +49,169 @@ public class AVAGeneralAgregateTest {
 
     @Test
     public void testLoadFilePurchaseDaily() {
-        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_daily").toArray(new String[0]);
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_daily").toArray(new String[0]);
         Assert.assertEquals(17, actual.length);
-        String[] age = hiveShell.executeQuery("SELECT age FROM agg_purchases_daily").toArray(new String[0]);
-//        System.out.println(age);
-        String[] age_is_not_null = hiveShell.executeQuery("SELECT age FROM agg_purchases_daily WHERE age IS NOT NULL").toArray(new String[0]);
+    // assert when age is not NULL and NULL
+        String[] age_is_not_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_daily WHERE age IS NOT NULL").toArray(new String[0]);
         Assert.assertNotNull(age_is_not_null);
-//		for (String string : age_is_not_null) {
-//			System.out.println(">>>>>>>>" + string);
-//		}
-        String[] age_is_null = hiveShell.executeQuery("SELECT age FROM agg_purchases_daily WHERE age IS NULL").toArray(new String[0]);
+        String[] age_is_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_daily WHERE age IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(age_is_not_null,age_is_null);
+    // assert when paymenttype is not NULL and NULL
+        String[] paymenttype_is_not_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_daily WHERE paymenttype IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(paymenttype_is_not_null);
+        String[] paymenttype_is_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_daily WHERE paymenttype IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(paymenttype_is_not_null,paymenttype_is_null);
+    // assert when currency is not NULL and NULL
+        String[] currency_is_not_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_daily WHERE currency IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(currency_is_not_null);
+        String[] currency_is_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_daily WHERE currency IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(currency_is_not_null,currency_is_null);
+    // assert when revenues is not NULL and NULL
+        String[] revenues_is_not_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_daily WHERE revenues > 0").toArray(new String[0]);
+        Assert.assertNotNull(revenues_is_not_null);
+        String[] revenues_is_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_daily WHERE revenues != 0").toArray(new String[0]);
+        Assert.assertNotEquals(revenues_is_not_null, revenues_is_null);
+    // assert when purchases  is not NULL and NULL
+        String[] purchases_is_not_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_daily WHERE purchases IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(purchases_is_not_null);
+        String[] purchases_is_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_daily WHERE purchases IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(purchases_is_not_null,purchases_is_null);
+    }
+
+    @Test
+    public void testLoadFilePurchaseWeekly() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_weekly").toArray(new String[0]);
+        Assert.assertEquals(17, actual.length);
+    // assert when age is not NULL and NULL
+        String[] age_is_not_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_weekly WHERE age IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(age_is_not_null);
+        String[] age_is_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_weekly WHERE age IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(age_is_not_null,age_is_null);
+    // assert when paymenttype is not NULL and NULL
+        String[] paymenttype_is_not_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_weekly WHERE paymenttype IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(paymenttype_is_not_null);
+        String[] paymenttype_is_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_weekly WHERE paymenttype IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(paymenttype_is_not_null,paymenttype_is_null);
+    // assert when currency is not NULL and NULL
+        String[] currency_is_not_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_weekly WHERE currency IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(currency_is_not_null);
+        String[] currency_is_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_weekly WHERE currency IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(currency_is_not_null,currency_is_null);
+    // assert when revenues is not NULL and NULL
+        String[] revenues_is_not_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_weekly WHERE revenues > 0").toArray(new String[0]);
+        Assert.assertNotNull(revenues_is_not_null);
+        String[] revenues_is_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_weekly WHERE revenues != 0").toArray(new String[0]);
+        Assert.assertNotEquals(revenues_is_not_null, revenues_is_null);
+    // assert when purchases  is not NULL and NULL
+        String[] purchases_is_not_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_weekly WHERE purchases IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(purchases_is_not_null);
+        String[] purchases_is_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_weekly WHERE purchases IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(purchases_is_not_null,purchases_is_null);
+    }
+
+    @Test
+    public void testLoadFilePurchaseMonth() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_monthly").toArray(new String[0]);
+        Assert.assertEquals(17, actual.length);
+
+    // assert when age is not NULL and NULL
+        String[] age_is_not_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_monthly WHERE age IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(age_is_not_null);
+        String[] age_is_null = hiveShell.executeQuery(
+                "SELECT age FROM agg_purchases_monthly WHERE age IS NULL").toArray(new String[0]);
         Assert.assertNotEquals(age_is_not_null,age_is_null);
 
+    // assert when paymenttype is not NULL and NULL
+        String[] paymenttype_is_not_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_monthly WHERE paymenttype IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(paymenttype_is_not_null);
+        String[] paymenttype_is_null = hiveShell.executeQuery(
+                "SELECT paymenttype FROM agg_purchases_monthly WHERE paymenttype IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(paymenttype_is_not_null,paymenttype_is_null);
+
+    // assert when currency is not NULL and NULL
+        String[] currency_is_not_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_monthly WHERE currency IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(currency_is_not_null);
+        String[] currency_is_null = hiveShell.executeQuery(
+                "SELECT currency FROM agg_purchases_monthly WHERE currency IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(currency_is_not_null,currency_is_null);
+
+    // assert when revenues is not NULL and NULL
+        String[] revenues_is_not_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_monthly WHERE revenues > 0").toArray(new String[0]);
+        Assert.assertNotNull(revenues_is_not_null);
+        String[] revenues_is_null = hiveShell.executeQuery(
+                "SELECT revenues FROM agg_purchases_monthly WHERE revenues != 0").toArray(new String[0]);
+        Assert.assertNotEquals(revenues_is_not_null, revenues_is_null);
+
+    // assert when purchases  is not NULL and NULL
+        String[] purchases_is_not_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_monthly WHERE purchases IS NOT NULL").toArray(new String[0]);
+        Assert.assertNotNull(purchases_is_not_null);
+        String[] purchases_is_null = hiveShell.executeQuery(
+                "SELECT purchases FROM agg_purchases_monthly WHERE purchases IS NULL").toArray(new String[0]);
+        Assert.assertNotEquals(purchases_is_not_null,purchases_is_null);
     }
-    /**
-     @Test
-     public void testPurhasePartition() {
-     String[] actual = hiveShell.executeQuery("SELECT username FROM purchase p WHERE p.partition_date='20080815'")
-     .toArray(new String[0]);
-     assertThat(actual, hasItemInArray("sotnaskrik"));
-     assertThat(actual, hasItemInArray("modosirron"));
-     }
 
-     @Test
-     public void testPurhaseUserId() {
-     String[] actual1 = hiveShell.executeQuery("SELECT userid FROM first_purchase_user_list f WHERE f.partition_date='20080815'")
-     .toArray(new String[0]);
-     assertThat(actual1, hasItemInArray("300000001"));
+    @Test
+    public void testLoadFilePurchaseUserList() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM purchase_user_list").toArray(new String[0]);
+        Assert.assertEquals(18, actual.length);
+    }
 
-     }
+    @Test
+    public void testLoadFileFirstPurchaseUserList() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM first_purchase_user_list").toArray(new String[0]);
+        Assert.assertEquals(14, actual.length);
+    }
 
+    @Test
+    public void testLoadFilePurchaseDailyUserButton() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_daily_user_button").toArray(new String[0]);
+        Assert.assertEquals(12, actual.length);
+    }
 
-     */
+    @Test
+    public void testLoadFilePurchaseWeeklyUserButton() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_weekly_user_button").toArray(new String[0]);
+        Assert.assertEquals(12, actual.length);
+    }
 
-//    @Test
-//    public void testLoadFilePurchaseWeekly() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_weekly").toArray(new String[0]);
-//        Assert.assertEquals(17, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFilePurchaseMonth() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_monthly").toArray(new String[0]);
-//        Assert.assertEquals(17, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFilePurchaseUserList() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM purchase_user_list").toArray(new String[0]);
-//        Assert.assertEquals(18, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFileFirstPurchaseUserList() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM first_purchase_user_list").toArray(new String[0]);
-//        Assert.assertEquals(14, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFilePurchaseDailyUserButton() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_daily_user_button").toArray(new String[0]);
-//        Assert.assertEquals(12, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFilePurchaseWeeklyUserButton() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_weekly_user_button").toArray(new String[0]);
-//        Assert.assertEquals(12, actual.length);
-//    }
-//
-//    @Test
-//    public void testLoadFilePurchaseMonthUserButton() {
-//        String[] actual = hiveShell.executeQuery("SELECT * FROM agg_purchases_monthly_user_button").toArray(new String[0]);
-//        Assert.assertEquals(12, actual.length);
-//    }
+    @Test
+    public void testLoadFilePurchaseMonthUserButton() {
+        String[] actual = hiveShell.executeQuery(
+                "SELECT * FROM agg_purchases_monthly_user_button").toArray(new String[0]);
+        Assert.assertEquals(12, actual.length);
+    }
+
 
 }
